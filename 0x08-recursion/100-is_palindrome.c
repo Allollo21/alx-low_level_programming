1d41830c04a490f6386e606d168c9d5d62ef6729
Allollo21 <Aliredaer7@gmail.com>
2023-10-25 16:01:31 -0700

    modified

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,37 +1,48 @@
 #include "main.h"
 
 /**
- *is_prime_number - says if integer prime or not
- *@n: number
- *Return: always 0
+ *_strlen_recursion - returns the length of a string.
+ *@s: string
+ *Return: the length of a string.
  */
-int is_prime_number(int n)
-{
-	if (n <= 1)
-	{
-		return (0);
-	}
 
-	return (actual_prime(n, n - 1));
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
 
 /**
- *actual_prime - calculates if number is prime recursively
- *@n: number
- *@i: iterator
- *Return: always 0
+ *comparator - compares each character of the string.
+ *@s: string
+ *@n1: smallest iterator.
+ *@n2: biggest iterator.
+ *Return: .
  */
-int actual_prime(int n, int i)
+
+int comparator(char *s, int n1, int n2)
 {
-	if (i == 1)
+	if (*(s + n1) == *(s + n2))
 	{
+		if (n1 == n2 || n1 == n2 + 1)
+			return (1);
+		return (0 + comparator(s, n1 + 1, n2 - 1));
+	}
+
+	return (0);
+}
+
+/**
+ *is_palindrome - detects if a string is a palindrome.
+ *@s: string.
+ *Return: 1 if s is a palindrome, 0 if not.
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
 		return (1);
-	}
-
-	if (n % i == 0 && i > 0)
-	{
-		return (0);
-	}
-
-	return (actual_prime(n, i - 1));
+	return (comparator(s, 0, _strlen_recursion(s) - 1));
 }
