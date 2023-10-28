840c0b14a4b46536268d7e4e5946bc11af537f65
Allollo21 <Aliredaer7@gmail.com>
2023-10-27 17:44:22 -0700

    modified

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,70 +2,19 @@
 #include <stdio.h>
 
 /**
- *_atoi - converts string into integer
- *@s: string
- *Return: always 0
- */
-int _atoi(char *s)
-{
-	int x, y, z, len, n, digit;
-
-	x = 0;
-	y = 0;
-	z = 0;
-	len = 0;
-	n = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	while (x < len && n == 0)
-	{
-		if (s[x] == '-')
-			++y;
-		if (s[x] >= '0' && s[x] <= '9')
-		{
-			digit = s[x] - '0';
-			if (y % 2)
-				digit = -digit;
-			z = z * 10 + digit;
-			n = 1;
-			if (s[x + 1] < '0' || s[x + 1] > '9')
-				break;
-			n = 0;
-		}
-
-		x++;
-	}
-
-	if (n == 0)
-		return (0);
-
-	return (z);
-}
-
-/**
- *main - multiplies two numbers
+ *main - entry point
  *@argc: number of arguments
  *@argv: array of arguments
  *Return: always 0
  */
 int main(int argc, char *argv[])
 {
-	int result, num1, num2;
+	int x;
 
-	if (argc < 3 || argc > 3)
+	for (x = 0; x < argc; x++)
 	{
-		printf("Error\n");
-		return (1);
+		printf("%s\n", argv[x]);
 	}
-
-	num1 = _atoi(argv[1]);
-	num2 = _atoi(argv[2]);
-	result = num1 * num2;
-
-	printf("%d\n", result);
 
 	return (0);
 }
