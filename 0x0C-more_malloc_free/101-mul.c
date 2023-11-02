32fabe38f8f8fbce91fd047d99b0b094aae39af5
Allollo21 <Aliredaer7@gmail.com>
2023-11-02 10:49:43 -0700

    modified

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,50 +1,39 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 /**
- *is_digit - Check if a string is composed of digits
- *@str: The string to check
- *Return: 1 if composed of digits, 0 otherwise
- */
-int is_digit(char *str)
-{
-	while (*str)
-	{
-		if (*str < '0' || *str > '9')
-		    return (0);
-		str++;
-	}
+ *main - multiplies two positive numbers
+ *@argc: n arguments
+ *@argv: args
+ *Return: int
+ **/
 
-	return (1);
-}
-
-/**
- *main - Multiply two positive numbers
- *@argc: Number of arguments
- *@argv: Array of arguments
- *Return: 0 on success, 98 on error
- */
 int main(int argc, char *argv[])
 {
-	if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
+	unsigned long mul;
+	int i, j;
+
+	if (argc != 3)
 	{
 		printf("Error\n");
-		return (98);
+		exit(98);
 	}
 
-	char *num1 = argv[1], *num2 = argv[2];
-	int len_result = 0, len1 = 0, len2 = 0;
-
-	while (num1[len1] || num2[len2])
+	for (i = 1; i < argc; i++)
 	{
-		int product = (len1 >= 0 ? num1[len1] - '0' : 0) * (num2[len2] - '0');
-
-		len_result += product;
-		len_result = len_result / 10 + (len_result % 10);
-		len1 -= 1;
-		len2 += 1;
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
 	}
 
-	printf("%d\n", len_result);
+	mul = atol(argv[1]) * atol(argv[2]);
+	printf("%lu\n", mul);
 	return (0);
 }
