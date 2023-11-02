4870dbcbe1d92bd8ccb5aba818774b2d11fdbd68
Allollo21 <Aliredaer7@gmail.com>
2023-11-02 10:20:01 -0700

    modified

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,26 +1,35 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- *main - prints the product of two arguments received.
- *@argc: type int argument
- *@argv: type char argument of string.
- *Return: if not receive 2 arg, rt error
+ *main - multiplies two positive numbers
+ *@argc: n arguments
+ *@argv: args
+ *Return: int
  */
 int main(int argc, char *argv[])
 {
-	int a, b, prod;
+	unsigned long mul;
+	int i, j;
 
 	if (argc != 3)
-		printf("Error\n");
-	else
 	{
-		a = atoi(argv[1]);
-		b = atoi(argv[2]);
-		prod = a * b;
-		printf("%d\n", prod);
+		printf("Error\n");
+		exit(98);
 	}
 
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+	}
+
+	mul = atol(argv[1]) * atol(argv[2]);
+	printf("%lu\n", mul);
 	return (0);
 }
